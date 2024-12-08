014ce23c99106719caf790cae3e7986eb61e756e
edsim97 <edsim97@yandex.ru>
2024-12-08 18:35:37 +0300

    Репозитории банков

---
+++ src/main/java/pro/entera/resource_service/repositories/BankKazRepository.java
@@ -0,0 +1,49 @@
+package pro.entera.resource_service.repositories;
+
+import org.hibernate.NonUniqueResultException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.entera.resource_service.models.BankKaz;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Репозиторий для доступа к модели {@link BankKaz}.
+ */
+public interface BankKazRepository extends JpaRepository<BankKaz, UUID> {
+    //region Public
+
+    /**
+     * Выполняет поиск банка Казахстана по заданному РНН'у.</p>
+     *
+     * @param rnn РНН банка Казахстана.
+     *
+     * @return Эталонный банк Казахстана.
+     *
+     * @throws NonUniqueResultException Если по РНН'у найдено болеее одного банка Казахстана.
+     */
+    Optional<BankKaz> findByRnn(String rnn);
+
+    /**
+     * Выполняет поиск банка Казахстана по заданному БИК'у.</p>
+     *
+     * @param bic БИК банка Казахстана.
+     *
+     * @return Эталонный банк Казахстана.
+     *
+     * @throws NonUniqueResultException Если по БИК'у найдено болеее одного банка Казахстана.
+     */
+    Optional<BankKaz> findByBIC(String bic);
+
+    /**
+     * Поиск не аннулированных банков Казахстана в текущей базе.</p>
+     *
+     * Не аннулированным считаеются те банки Казахстана у которых нет даты аннулирования.
+     *
+     * @return Список не аннулированных банков Казахстана.
+     */
+    List<BankKaz> findByAnnullingDateIsNull();
+
+    //endregion
+}
---
+++ src/main/java/pro/entera/resource_service/repositories/BankRusRepository.java
@@ -0,0 +1,38 @@
+package pro.entera.resource_service.repositories;
+
+import org.hibernate.NonUniqueResultException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.entera.resource_service.models.BankRus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * <p>Интерфейс репозитория для доступа к модели {@link BankRus}.</p>
+ */
+public interface BankRusRepository extends JpaRepository<BankRus, UUID> {
+    //region Public
+
+    /**
+     * <p>Выполняет поиск банка по заданному БИК'у.</p>
+     *
+     * @param bic БИК банка.
+     *
+     * @return Эталонный банк.
+     *
+     * @throws NonUniqueResultException Если по БИК'у найдено болеее одного банка.
+     */
+    Optional<BankRus> findByBIC(String bic);
+
+    /**
+     * <p>Поиск не аннулированных банков в текущей базе.</p>
+     *
+     * <p>Не аннулированным считаеются те банки у которых нет даты аннулирования.</p>
+     *
+     * @return Список не аннулированных банков.
+     */
+    List<BankRus> findByAnnullingDateIsNull();
+
+    //endregion
+}
